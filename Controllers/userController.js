508606e7004e7a675a189c4950848f65a76d5d8f
Sayena <sayena0704@gmail.com>
2024-07-25 16:17:37 +0530

    logout changes

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -78,6 +78,9 @@ try {
 export const logout = catchAsyncError(async(req, res, next) =>{
     res.status(200).cookie("token", null, {
         expires: new Date(Date.now()),
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
     }).json({
         success:true,
         message: "Logged Out Successfully",
